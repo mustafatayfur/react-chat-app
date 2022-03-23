951a7b1bb4b24da8d83dd3d037796ae70c7b9b09
mustafa <mltm0204@gmail.com>
2022-03-24 00:13:01 +0300

    signup input

--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -12,14 +12,14 @@ const Homepage = () => {
                 justifyContent="center"
                 p={3}
                 bg={"white"}
-                w="180%"
+                w="150%"
                 m="40px 0 15px 0"
                 borderRadius="lg"
                 borderWidth="1px"
                 >
                 <Text fontSize='4xl' fontFamily="work sans" color="purple">Chat-OE</Text>
             </Box>
-            <Box bg="white" w="180%" p={4} borderRadius="lg" borderWidth="1px" >
+            <Box bg="white" w="150%" p={4} borderRadius="lg" borderWidth="1px" >
             <Tabs variant='soft-rounded' colorScheme='purple'>
             <TabList mb="1em">
               <Tab width="50%">Login</Tab>
--- src/components/Authentication/Signup.js
+++ src/components/Authentication/Signup.js
@@ -1,8 +1,21 @@
-import React from 'react'
+import { FormControl, FormLabel, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
 
 const Signup = () => {
+    const [name, setName]= useState()
+
   return (
-    <div>Signup</div>
+    <VStack spacing="5px">
+        <FormControl>
+            <FormLabel>
+            
+            </FormLabel>
+            <Input 
+                placeholder='Enter Your Name'
+                onChange={(e)=>setName(e.target.value)}
+                />
+        </FormControl>
+    </VStack>
   )
 }
 
